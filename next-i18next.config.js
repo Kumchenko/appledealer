7d7351af573a80cf6ecba546091889f2ca1cc9ec
Kirill Kumchenko <kirillkumchenko@gmail.com>
2023-06-19 23:09:19 +0300

    Fix Back-End req and improve dev config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,6 +3,19 @@
 module.exports = {
     i18n: {
         locales: ['uk', 'ru', 'en'],
+<<<<<<< Updated upstream
         defaultLocale: 'uk'
     }
+=======
+        defaultLocale: 'uk',
+    },
+    localePath:
+        typeof window === 'undefined'
+            ? require('path').resolve('./public/locales')
+            : './public/locales',
+    reloadOnPrerender: 
+        process.env.NODE_ENV === 'production'
+            ? false
+            : true
+>>>>>>> Stashed changes
 }
