0fd73f764c456e047797c747ca0570283b7aa04e
Kirill Kumchenko <kirillkumchenko@gmail.com>
2023-06-19 21:30:24 +0300

    Fix translation

--- i18next.d.ts
+++ i18next.d.ts
@@ -3,6 +3,13 @@ import "i18next";
 // import all namespaces (for the default language, only)
 import common from "locales/uk/common.json";
 import check from "locales/uk/check.json";
+import error from "locales/uk/error.json";
+import index from "locales/uk/index.json";
+import order from "locales/uk/order.json";
+import repair from "locales/uk/repair.json";
+import status from "locales/uk/status.json";
+import thanks from "locales/uk/thanks.json";
+import timer from "locales/uk/timer.json";
 
 declare module "i18next" {
     // Extend CustomTypeOptions
@@ -13,6 +20,13 @@ declare module "i18next" {
         resources: {
             common: typeof common;
             check: typeof check;
+            error: typeof error;
+            index: typeof index;
+            order: typeof order;
+            repair: typeof repair;
+            status: typeof status;
+            thanks: typeof thanks;
+            timer: typeof timer;
         };
         returnNull: false;
         // other
--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,6 +3,10 @@
 module.exports = {
     i18n: {
         locales: ['uk', 'ru', 'en'],
-        defaultLocale: 'uk'
-    }
+        defaultLocale: 'uk',
+    },
+    localePath:
+        typeof window === 'undefined'
+            ? require('path').resolve('./public/locales')
+            : './public/locales',
 }
